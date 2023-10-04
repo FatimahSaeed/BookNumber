6d318a66e0f73849e5302e721417af7cbf3a8ed3
FatimahSaeed <55546717+FatimahSaeed@users.noreply.github.com>
2023-10-04 20:54:33 +0300

    Update bookNumber.py

--- bookNumber.py
+++ bookNumber.py
@@ -1,3 +1,4 @@
+#create dictionary
 book_numbers = {"Amal": 1111111111,
                 "Mohammed": 2222222222,
                 "Khadijah": 3333333333,
@@ -6,15 +7,18 @@ book_numbers = {"Amal": 1111111111,
                 "Faisal": 6666666666,
                 "Layla": 7777777777}
 
+#get user inputs
 number = input("enter the number to search")
 newname = input("Enter new name")
 newnum = input("Enter new number")
 
+#add new items to the dictionary
 if newnum and newname is not None:
     book_numbers.update({newname: newnum})
     print(book_numbers)
 
 
+#search the dictionary
 def numfun(number):
     num = int(number)
     for k, v in book_numbers.items():
@@ -22,6 +26,7 @@ def numfun(number):
            return k
 
 
+#check if the number is valid
 if number == str:
     print("This is invalid number")
 elif len(number) == 10:
